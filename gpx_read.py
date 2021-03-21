3ba7028927c9e5be9761dac573b6bd79acb20ed0
Riemer17 <riemer.kerkstra@gmail.com>
2021-03-21 10:10:16 +0100

    deleted old files

--- gpx_read.py
+++
@@ -1,5 +0,0 @@
-from gpx_converter import Converter
-
-rundf = Converter(input_file='gpx_files/Heuvelloopjes_Amsterdamse_bos.gpx').gpx_to_dataframe()
-
-
--- plot.py
+++
@@ -1,5 +0,0 @@
-import matplotlib.pyplot as plt
-from gpx_read import rundf
-
-plt.plot(rundf['latitude'], rundf['longitude'])
-plt.show()
